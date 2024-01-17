45bf3478565baba925d83d52985d5279332af20f
Alaina Acton <alaina.acton@icloud.com>
2024-01-17 11:57:26 -0600

    Stopped at Part 1, step 6.

--- Course261_Course_Project_2.py
+++ Course261_Course_Project_2.py
@@ -1,1 +1,22 @@
+#Alaina Acton
+#CIS261
+#Course Project
+#3
+def getempName():
+    empName = input("Enter Employee Name: ")
+    return empName
+#4
+def getHoursWorked():
+    hours = float(input("Enter total amount of hours worked: "))
+    return hours
+#5
+def getHourlyRate():
+    rate = float(input("Enter hourly rate: "))
+    return rate
+#6    
+def getIncomeTax():
+    tax = float(input("Enter tax rate: "))
+    tax = IncomeTax / 100
+    return tax
+
 
