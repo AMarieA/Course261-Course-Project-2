15ccffa31ad71a04fdf1912e811226ddeff6555f
Alaina Acton <alaina.acton@icloud.com>
2024-02-01 22:08:01 -0600

    Problems will need to be fixed

--- Course261_Course_Project_2.py
+++ Course261_Course_Project_2.py
@@ -1,63 +1,91 @@
 #Alaina Acton
 #CIS261
 #Course Project
-#3
+
+def getdates():
+    fromDate = (input("Enter date started in mm/dd/yyyy format: "))
+    endDate = (input("Enter date ended in mm/dd/yyyy format: "))
+    return fromDate, endDate
+    
 def getempName():
     empName = input("Enter Employee Name: ")
     return empName
-#4
+
 def getHoursWorked():
     hours = float(input("Enter total amount of hours worked: "))
     return hours
-#5
+
 def getHourlyRate():
     rate = float(input("Enter hourly rate: "))
     return rate
-#6    
-def getIncomeTax():
+    
+def getTaxRate():
     tax = float(input("Enter tax rate: "))
     tax = tax / 100
     return tax
-#7
+
 def CalcTaxAndNetpay(hours, rate, tax):
     gPay = hours * rate
     incomeTax = gPay * tax
     netPay = gPay - incomeTax
     return gPay, incomeTax, netPay
 
-def printinfo(empName, hours, rate, gPay, tax, incomeTax, netPay):
-    print(empName, f"{hours:,.2f}", f"{rate:.2f}", f"{gPay:.2f}", f"{tax:.1%}", f"{incomeTax:,.2f}", f"{netPay:,.2f}")
+def printinfo(empDetailList):
+    totalEmployees = 0
+    totalHours = 0.00
+    totalGrossPay = 0.00
+    totalTax = 0.00
+    totalNetPay = 0.00
+    for empList in empDetailList:
+        fromDate = empList[0]
+        endDate = empList[1]
+        empName = empList[2]
+        hours = empList[3]
+        rate = empList[4]
+        tax = empList[5]
+        
+        grosspay, incomeTax, netPay = CalcTaxAndNetpay(hours, rate, tax)
+        print(fromDate, endDate, empName, f"{hours:,.2f}", f"{rate:.2f}", f"{grosspay:.2f}", f"{tax:.1%}", f"{incomeTax:,.2f}", f"{netPay:,.2f}")
+        
+        totalEmployees += 1
+        totalHours += hours
+        totalGrossPay += grosspay
+        totalTax += incomeTax
+        totalNetPay += netPay
+        
+        empTotals['totEmp'] = totalEmployees
+        empTotals['totHours'] = totalHours
+        empTotals['totGross'] = totalGrossPay
+        empTotals['totTax'] = totalTax
+        empTotals['totNet'] = totalNetPay
 
-def PrintTotals(totalEmp, totalHours, totalGpay, totalTax, totalNetpay):
-    print(f"\nTotal numer of employees: {totalEmp}")
-    print(f"Total hours: {totalHours:,.2f}")
-    print(f"Total Gross Pay: {totalGpay:,.2f}")
-    print(f"Total Tax: {totalTax:,.2f}")
-    print(f"Total Net Pay: {totalNetpay:,.2f}")
+def PrintTotals(empTotals):
+    print(f"\nTotal numer of employees: {empTotals['totEmp']}")
+    print(f"Total hours: {empTotals['totHours']}")
+    print(f"Total Gross Pay: {empTotals['totGross']}")
+    print(f"Total Tax: {empTotals['totTax']}")
+    print(f"Total Net Pay: {empTotals['totNet']}")
     
 if __name__ == "__main__":
-    totalEmp = 0
-    totalHours = 0.00
-    totalGpay = 0.00
-    totalTax = 0.00
-    totalNetpay = 0.00
-    
-    while True:
-        empName = getempName()
-        if (empName.upper() == "END"):
-            break
-        hours = getHoursWorked()
-        rate = getHourlyRate()
-        tax = getIncomeTax()
-        gPay, incomeTax, netPay = CalcTaxAndNetpay(hours, rate, tax)
+   empDetailList = []
+   empTotals = {}
+   while True:
+       empName = getempName()
+       if (empName.upper() == "END"):
+           break
+       fromDate, EndDate = getdates()
+       hours = getHoursWorked()
+       rate = getHourlyRate()
+       tax = getTaxRate()
+       empDetail = []
+       empDetail.insert(0, fromDate)
+       empDetail.insert(1, EndDate)
+       empDetail.insert(2, empName)
+       empDetail.insert(3, hours)
+       empDetail.insert(4, rate)
+       empDetail.insert(5, tax)
+       empDetailList.append(empDetail)
         
-        printinfo(empName, hours, rate, gPay, tax, incomeTax, netPay)
-        
-        totalEmp += 1
-        totalHours += hours
-        totalGpay +=  gPay
-        totalTax += incomeTax
-        totalNetpay += netPay
-        
-PrintTotals(totalEmp, totalHours, totalGpay, totalTax, totalNetpay)
+       printinfo(empDetailList)
+       PrintTotals(empTotals)
         
